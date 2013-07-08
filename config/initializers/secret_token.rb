2c2e668e6490b21d18051c6b4fd4e8ce4aab6e03
Wesley Carr <surfswell17@gmail.com>
2013-07-08 14:20:38 -0400

    Updated gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dawgs::Application.config.secret_token = '6326223a602e7feec72c086385b0b376e0b57a48faba21b758722c6d8c90dab2bc39b49722dba89e07266fa9ce69c29888146531495f470473a5329e02741cf9'
