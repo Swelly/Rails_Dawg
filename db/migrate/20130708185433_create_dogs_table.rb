aea5eac49ef9bf459b8a6b188f136502677cccf3
Wesley Carr <surfswell17@gmail.com>
2013-07-08 14:56:14 -0400

    Created migration file for dogs. Added table

---
+++ db/migrate/20130708185433_create_dogs_table.rb
@@ -0,0 +1,15 @@
+class CreateDogsTable < ActiveRecord::Migration
+
+  def up
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :dogs
+  end
+end
