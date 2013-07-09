363b541dc8bd001ecbf121996dc99a3e279254fc
Wesley Carr <surfswell17@gmail.com>
2013-07-08 20:04:40 -0400

    Finished and fixed routing errors

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,13 +1,40 @@
 class DogsController < ApplicationController
   def index
+    @dogs = Dog.all
   end
 
-  def dogs
+  def new
   end
 
   def create
-    @name = params[:name]
-    @breed = params[:breed]
+    dog = Dog.new
+
+    dog.name =  params[:name]
+    dog.save
+
+    redirect_to('/dogs')
   end
 
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def destroy
+    Dog.find(params[:id]).delete
+
+    redirect_to('/dogs')
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    dog = Dog.find(params[:id])
+
+    dog.name = params[:name]
+    dog.save
+
+    redirect_to('/dogs')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Dawgs::Application.routes.draw do
-   root :to => 'welcome#index'
-   get '/dogs' => 'dogs#index'
-   get '/dogs/new' => 'dogs#dogs'
-   post '/dogs' => 'dogs#create'
+
+ root :to => 'welcome#index'
+ get '/dogs' => 'dogs#index'
+ get '/dogs/new' => 'dogs#new'
+ post '/dogs/create' => 'dogs#create'
+ get '/dogs/:id' => 'dogs#show'
+ put '/dogs/:id' => 'dogs#update'
+ post '/dogs/:id/delete' => 'dogs#destroy'
+ get '/dogs/:id/edit' => 'dogs#edit'
+
 end
